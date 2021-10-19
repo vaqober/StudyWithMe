7c74b9746197484c76fd6e6e53b1cf455d854384
Rasul Yunusov <rasul.yunusov282750@gmail.com>
2021-10-19 21:02:19 +0300

    Add pagerView2, pagerViewAdapter

--- app/src/main/java/com/studywithme/app/LoginActivity.kt
+++ app/src/main/java/com/studywithme/app/LoginActivity.kt
@@ -2,10 +2,35 @@ package com.studywithme.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+
+        viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+
+        TabLayoutMediator(tabLayout, viewPager) {
+            tab, position ->
+            when (position) {
+                0 -> {tab.text = "Login"}
+                1 -> {tab.text = "Sign Up"}
+                else -> {tab.text = "Login"}
+            }
+        }.attach()
     }
+
 }
+
--- app/src/main/java/com/studywithme/app/ViewPagerAdapter.kt
+++ app/src/main/java/com/studywithme/app/ViewPagerAdapter.kt
@@ -1,4 +1,22 @@
 package com.studywithme.app
 
-class ViewPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fm: FragmentManager, lifecycle: Lifecycle)
+    :  FragmentStateAdapter(fm, lifecycle) {
+    override fun getItemCount(): Int {
+        return 2;
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when(position) {
+            0 -> return LoginFragment()
+            1 -> return SignUpFragment()
+            else -> return LoginFragment()
+        }
+    }
 }

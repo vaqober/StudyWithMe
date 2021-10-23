ffba0e58b8838a64de1082b962eaa35c6ba9106c
Rasul Yunusov <rasul.yunusov282750@gmail.com>
2021-10-23 12:32:31 +0300

    Fix MainActivity

--- app/src/main/java/com/studywithme/app/MainActivity.kt
+++ app/src/main/java/com/studywithme/app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.studywithme.app
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

97dd5a4d3ad625d5534800990953832a8fcb588f
Vonisak <ivan.kasinov@mail.ru>
2021-11-13 14:36:07 +0300

    refactor create room to mvp

--- app/src/main/java/com/studywithme/app/present/fragments/CreateRoomFragment.kt
+++ app/src/main/java/com/studywithme/app/present/fragments/CreateRoomFragment.kt
@@ -9,17 +9,23 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.isVisible
 import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.studywithme.app.MockDataStore
 import com.studywithme.app.R
 import com.studywithme.app.databinding.FragmentCreateRoomBinding
+import com.studywithme.app.objects.room.Room
 import com.studywithme.app.objects.room.RoomDto
+import com.studywithme.app.present.State
+import com.studywithme.app.present.models.CreateRoomViewModel
 import kotlinx.coroutines.launch
 
 class CreateRoomFragment : Fragment() {
 
+    private val viewModel by viewModels<CreateRoomViewModel>()
     private var _binding: FragmentCreateRoomBinding? = null
     private val binding get() = _binding!!
     private var imageUri = Uri.parse(
@@ -36,6 +42,7 @@ class CreateRoomFragment : Fragment() {
         setCreateButtonSettings()
         setThemeTextFieldSettings()
         setPhotoPickButtonSettings()
+        observeModel()
 
         return binding.root
     }
@@ -54,7 +61,7 @@ class CreateRoomFragment : Fragment() {
                 return@setOnClickListener
             } else {
                 lifecycleScope.launch {
-                    MockDataStore.postRoom(room)
+                    viewModel.postRoom(room)
                 }
             }
         }
@@ -89,6 +96,19 @@ class CreateRoomFragment : Fragment() {
     private fun setPhotoPickButtonSettings() {
         binding.addPhotoButton.setOnClickListener {
             selectImageFromGalleryResult.launch("image/*")
+        }
+    }
+
+    private fun observeModel() {
+        viewModel.getState().observe(viewLifecycleOwner) {
+            when (it) {
+                is State.Fail -> {
+                    Toast.makeText(requireContext(), "Fail: ${it.error}", Toast.LENGTH_LONG).show()
+                }
+                is State.Success -> {
+                    Toast.makeText(requireContext(), "Success: ${it.data}", Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
 
---
+++ app/src/main/java/com/studywithme/app/present/models/CreateRoomViewModel.kt
@@ -0,0 +1,50 @@
+package com.studywithme.app.present.models
+
+import android.os.Handler
+import android.os.Looper
+import androidx.core.os.postDelayed
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.studywithme.app.business.providers.IRoomProvider
+import com.studywithme.app.business.providers.Result
+import com.studywithme.app.objects.AbstractRoom
+import com.studywithme.app.objects.room.RoomDto
+import com.studywithme.app.present.State
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class CreateRoomViewModel: ViewModel(), KoinComponent {
+    private val handler = Handler(Looper.getMainLooper())
+    private val provider by inject<IRoomProvider>()
+    private val state = MutableLiveData<State<AbstractRoom>>()
+
+    var query = ""
+        private set
+
+    fun getState(): LiveData<State<AbstractRoom>> = state
+
+    fun postRoom(room: RoomDto) {
+        postponedQuery(room)
+    }
+
+    private fun postponedQuery(room: RoomDto) {
+        handler.removeCallbacksAndMessages(null)
+        handler.postDelayed(delayInMillis = 600) {
+            makeRequest(room)
+        }
+    }
+
+    private fun makeRequest(room: RoomDto) {
+        state.postValue(State.Pending())
+
+        provider.postRoom(room) {
+            val newState = when (it) {
+                is Result.Success -> State.Success(it.data)
+                is Result.Fail -> State.Fail(it.error)
+            }
+
+            state.postValue(newState)
+        }
+    }
+}

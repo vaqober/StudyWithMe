54b04f13abd6128d5087e97d76b007a7317980cb
vaqober <vaqober@mail.ru>
2021-11-17 16:57:36 +0300

    fix ktlint

--- app/src/main/java/com/studywithme/app/business/providers/UserProvider.kt
+++ app/src/main/java/com/studywithme/app/business/providers/UserProvider.kt
@@ -3,9 +3,7 @@ package com.studywithme.app.business.providers
 import com.studywithme.app.datalayer.accessors.IUserAccessor
 import com.studywithme.app.objects.AbstractUser
 import java.lang.IllegalStateException
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class UserProvider(private val onlineAccessor: IUserAccessor) :
     AbstractCoroutineProvider, IUserProvider {

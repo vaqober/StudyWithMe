0ba1a8e1a60b575e9d7bdcf586de8a00fcfdb2d7
rivavl <mvkalinenkova@yandex.ru>
2021-12-15 07:00:02 +0300

    ktlint detekt

--- app/src/main/java/com/studywithme/app/datalayer/accessors/IUserAccessor.kt
+++ app/src/main/java/com/studywithme/app/datalayer/accessors/IUserAccessor.kt
@@ -2,7 +2,11 @@ package com.studywithme.app.datalayer.accessors
 
 import com.studywithme.app.objects.user.User
 import com.studywithme.app.objects.user.UserList
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface IUserAccessor {
     @GET("users.json")
